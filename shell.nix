03d064cde18c88031dffb01685a745f502788b59
Ben Weintraub <ben@weintraub.xyz>
2021-11-13 10:57:59 -0500

    Clean up shell.nix

--- shell.nix
+++ shell.nix
@@ -187,11 +187,6 @@ let
   };
   pythonPackages = ps:
     with ps; [
-      # This execute some shell code to initialize a venv in $venvDir before
-      # dropping into the shell
-      # venvShellHook
-
-      # wheel
       setuptools
 
       # Linting
@@ -213,20 +208,6 @@ in mkShell rec {
 
     # Python development
     (python38.withPackages pythonPackages)
-    # pipreqs
+    pipreqs
   ];
-
-  # # Run this command, only after creating the virtual environment
-  # postVenvCreation = ''
-  #   unset SOURCE_DATE_EPOCH
-  #   for requirements_file in requirements*.txt; do
-  #     pip install -r $requirements_file
-  #   done
-  # '';
-
-  # postShellHook = ''
-  #   # allow pip to install wheels
-  #   unset SOURCE_DATE_EPOCH
-  # '';
-
 }
