57db0c70ca31d75bd1ad469393d177374314f9e4
Christof FERREIRA TORRES <freerider_1991@hotmail.com>
2022-09-19 15:39:10 +0200

    Updated notebooks in analysis

--- analysis/utils.py
+++ analysis/utils.py
@@ -1,66 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import json
-import logging
 import os
-import time
-import requests
+import logging
 
 from pymongo import MongoClient
-from pymongo.collection import Collection
-
-OUTPUT_DIR = "output"
-
-TRANSFER       = "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef" # ERC20 "Transfer"
-TOKEN_PURCHASE = "0xcd60aa75dea3072fbc07ae6d7d856b5dc5f4eee88854f5b4abf7b680ef8bc50f" # Uniswap V1 "TokenPurchase"
-ETH_PURCHASE   = "0x7f4091b46c33e918a0f3aa42307641d17bb67029427a5369e54b353984238705" # Uniswap V1 "ETHPurchase"
 
 MONGODB_ENDPOINT = os.getenv("MONGODB_ENDPOINT")
 MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
 MONGODB_USERNAME = os.getenv("MONGODB_USERNAME")
 
 log = logging.getLogger(__name__)
-
-class colors:
-    INFO = '\033[94m'
-    OK = '\033[92m'
-    FAIL = '\033[91m'
-    END = '\033[0m'
-
-def convert_wei_to_eth(wei_amount):
-    return wei_amount / 1e18
-
-def get_prices():
-    return requests.get("https://api.coingecko.com/api/v3/coins/ethereum/market_chart/range?vs_currency=usd&from=1392577232&to="+str(int(time.time()))).json()["prices"]
-
-def get_one_eth_to_usd(timestamp, prices):
-    timestamp *= 1000
-    one_eth_to_usd = prices[-1][1]
-    for index, _ in enumerate(prices):
-        if index < len(prices)-1:
-            if prices[index][0] <= timestamp and timestamp <= prices[index+1][0]:
-                return prices[index][1]
-    print("Could not find timestamp. Returning latest price instead.")
-    return one_eth_to_usd
-
-def request_debug_trace(connection, transaction_hash, custom_tracer=True, request_timeout=100, disable_stack=True, disable_memory=True, disable_storage=True):
-    data, tracer = None, None
-    if custom_tracer:
-        with open(os.path.join(os.path.dirname(__file__), 'call_tracer.js'), 'r') as file:
-            tracer = file.read().replace('\n', '')
-    if tracer:
-        data = json.dumps({"id": 1, "method": "debug_traceTransaction", "params": [transaction_hash, {"tracer": tracer, "timeout": str(request_timeout)+"s"}]})
-    else:
-        data = json.dumps({"id": 1, "method": "debug_traceTransaction", "params": [transaction_hash, {"disableStack": disable_stack, "disableMemory": disable_memory, "disableStorage": disable_storage}]})
-    headers = {"Content-Type": "application/json"}
-    connection.request('GET', '/', data, headers)
-    response = connection.getresponse()
-    if response.status == 200 and response.reason == "OK":
-        return json.loads(response.read())
-    return {"error": {"status": response.status, "reason": response.reason, "data": response.read().decode()}}
-
-
 
 def connect_to_mongodb():
     log.info("Connecting to MongoDB")
